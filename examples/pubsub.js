4fa9f84e1d753c4c0993366bbb5c1beabd151520
Trygve Lie <post@trygve-lie.com>
2018-01-19 10:00:07 +0100

    Updated example

--- examples/pubsub.js
+++ examples/pubsub.js
@@ -6,24 +6,37 @@ const smsg = new SocketMsg();
 
 const pubsub = async () => {
     const pub = smsg.tcp('pub');
-    const sub = smsg.tcp('sub');
+    const sub1 = smsg.tcp('sub');
+    const sub2 = smsg.tcp('sub');
+    const sub3 = smsg.tcp('sub');
 
-    sub.on('message', (msg) => {
-        console.log('sub msg:', msg);
+    sub1.on('message', (msg) => {
+        console.log(`subscriber 1 got message from publisher: ${msg}`);
+    });
+
+    sub2.on('message', (msg) => {
+        console.log(`subscriber 2 got message from publisher: ${msg}`);
+    });
+
+    sub3.on('message', (msg) => {
+        console.log(`subscriber 3 got message from publisher: ${msg}`);
     });
 
     const paddr = await pub.bind();
-    console.log(paddr);
+    console.log(`publisher bound to port ${paddr.port}`);
 
-    const saddr = await sub.connect(paddr);
-    console.log(saddr);
+    const saddr1 = await sub1.connect(paddr);
+    const saddr2 = await sub2.connect(paddr);
+    const saddr3 = await sub3.connect(paddr);
+    console.log(`subscribers connected to port ${paddr.port}`);
 
-    const pcli = pub.send('hello from publisher');
-    console.log('sent message to num clients', pcli);
+    const pcli = pub.send('ping');
+    console.log(`publiser published message to ${pcli} subscribers`);
 
-    const sdead = await sub.close();
-    const pdead = await pub.close();
-    console.log(sdead, pdead);
+    await sub1.close();
+    await sub2.close();
+    await sub3.close();
+    await pub.close();
 };
 
 pubsub();
